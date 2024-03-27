d92be47a5df67fe62775edb8f57a68807cbc2c53
alfiaah <141263250+alfiaah@users.noreply.github.com>
2024-03-27 17:33:30 +0530

    Add files via upload

---
+++ Java/CFrame.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class CFrame
+{
+    public static void main(String args[])
+    {
+        Frame obj=new Frame("Test...");
+        obj.setSize(500,500);
+        obj.setVisible(true);
+    }
+}

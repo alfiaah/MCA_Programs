53c3071c8e5e211539cde8ba5ee0e70d471e24e4
alfiaah <141263250+alfiaah@users.noreply.github.com>
2024-05-29 21:26:24 +0530

    seclarg.c

---
+++ c/seclarg.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int largest=a[0];
+    int slargest=a[1];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            largest=a[i];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<largest && a[i]>slargest)
+        {
+            slargest=a[i];
+        }
+    }
+    printf("%d",slargest);
+    
+}

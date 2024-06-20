26d03d3e6d34c1638b2d6d7e4809d58a476623be
alfiaah <141263250+alfiaah@users.noreply.github.com>
2024-06-20 09:41:21 +0530

    Add files via upload

---
+++ python/classes.py
@@ -0,0 +1,10 @@
+class sem3:
+        def __init__(self,name) :
+            self.name=name
+        def duties(self):
+            print("Attendence")
+            
+classrep=sem3("Revathi")
+placementrep=sem3("Gracen")
+print(placementrep.name)
+classrep.duties()
